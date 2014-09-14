5bbd24b201b02e5d2ebc11709a2bc5036382cddb
leomastakusuma <leomastakusuma@gmail.com>
2014-09-14 10:56:03 +0700

    bukutamumodels

--- application/controller/bukutamu.php
+++ application/controller/bukutamu.php
@@ -1,16 +1,53 @@
 <?php
 
 class bukutamu extends Controller{
+    private $_model = 'bukutamumodels';
+    
     public function index(){
-//        require 'application/templates/header.html';
-        require 'application/views/signup/test.html';
+     require 'application/templates/header.html';
+     require 'application/templates/menu.html';
+     require 'application/views/bukutamu/index.html';
+     require 'application/templates/footer.php';
        
        
         
     }
+    
+    public function getall(){
+        $model  = $this->loadModel($this->_model);
+        $getall = $model->getall();
+        echo'<pre>';
+        print_r($getall);
+    }
     public function save(){
-        print_r($_POST);
-//        $this->redirect('bukutamu');
+        $form       = $_POST;
+        $nama       = $form['nama'];
+        $email      = $form['email'];
+        $komentar   = $form['Komentar'];
+        $tanggal    = date('Y-m-d H-i-s');
+        
+        if(!empty($form)){
+            $error = array();
+            if(empty($nama)){
+                $error[]  = 'Nama Tidak Boleh Kosong';
+            }
+            if(empty($komentar)){
+                $error[]  = 'Komentar Tidak Boleh Kosong Tidak Boleh Kosong';
+            }
+            
+            if(count($error) > 0) {
+                $errormsg = $error;
+                require 'application/templates/header.html';
+                require 'application/templates/menu.html';
+                require 'application/views/bukutamu/index.html';
+                require 'application/templates/footer.php';
+            }
+            else {
+                $model  = $this->loadModel($this->_model);
+                $simpan = $model->save($nama,$email,$komentar,$tanggal);
+            }
+        }
+       
     }
     
     
---
+++ application/models/bukutamumodels.php
@@ -0,0 +1,37 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class bukutamumodels extends Models{
+    private $table = 'bukutamu';
+    
+    public function getall(){
+       $data = $this->select('*', $this->table);
+       return $data;
+    }
+    
+    public function save($nama,$email,$komentar,$tanggal){
+        $data = array(':nama'     => $nama,
+                      ':email'    => $email,
+                      ':komentar' => $komentar,
+                      ':tanggal'  => $tanggal
+                     );
+        $sql  = "INSERT INTO {$this->table}";
+        $sql .= " (nama , email,komentar, tanggal)";
+        $sql .= " VALUES ( :nama, :email, :komentar, :tanggal)";
+        $query = $this->db->prepare($sql);
+        $query->execute($data);      
+        
+    }
+    public function deletebukutamu($id){
+        $sql     = "DELETE FROM {$this->table} ";
+        $sql    .= " WHERE id_bukutamu = {$id}";
+        $query   = $this->db->prepare($sql);
+        $query->execute(array(":id_bukutamu" => $id));
+        
+    }
+}
